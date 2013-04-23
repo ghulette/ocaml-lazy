35855458ae6e449c3adf911ff223a64fa0ed5387
Geoff Hulette <ghulette@gmail.com>
2013-04-22 22:15:50 -0700

    Update

--- lazylist.mli
+++ lazylist.mli
@@ -1,13 +1,16 @@
-type 'a t = Nil | Cons of 'a * (unit -> 'a t)
-val hd : 'a t -> 'a
-val tl : 'a t -> 'a t
-val nth : 'a t -> int -> 'a
+type 'a t = Nil | Cons of 'a * 'a t Lazy.t
+
 val from_list : 'a list -> 'a t
 val to_list : 'a t -> 'a list
-val take : int -> 'a t -> 'a t
-val drop : int -> 'a t -> 'a t
 val iterate : ('a -> 'a) -> 'a -> 'a t
 val repeat : 'a -> 'a t
+
+val hd : 'a t -> 'a
+val tl : 'a t -> 'a t
+val length : 'a t -> int
+val nth : 'a t -> int -> 'a
+val take : int -> 'a t -> 'a t
+val drop : int -> 'a t -> 'a t
 val map : ('a -> 'b) -> 'a t -> 'b t
 val filter : ('a -> bool) -> 'a t -> 'a t
 val fold_left : ('a -> 'b -> 'a) -> 'a -> 'b t -> 'a
@@ -15,4 +18,5 @@ val fold_right : ('a -> 'b -> 'b) -> 'b -> 'a t -> 'b
 val unfold : ('a -> ('b * 'a) option) -> 'a -> 'b t
 val zip : 'a t -> 'b t -> ('a * 'b) t
 val unzip : ('a * 'b) t -> 'a t * 'b t
+
 val nats : int t
